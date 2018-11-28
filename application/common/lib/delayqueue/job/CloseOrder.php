c409780f1dc992d0faae11bc45173e9ba336c910
yunpeng <liyunpeng0601@163.com>
2018-11-28 10:09:33 +0800

    ...

--- application/common/lib/delayqueue/job/CloseOrder.php
+++ application/common/lib/delayqueue/job/CloseOrder.php
@@ -16,7 +16,7 @@ class CloseOrder extends DelayJob
     public function preform()
     {
         // payload 里应该有处理任务所需的参数，通过DelayQueue的addTask传入
-        //print_r('test job'.PHP_EOL);
+        print_r('test job'.PHP_EOL);
         $args = $this->payload;
         $order=Order::getOrderByID($args['order_id']);
         if($order->status == 1){
